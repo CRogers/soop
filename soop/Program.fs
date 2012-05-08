8c031604c15be875b3bc53c663b87b416ff8260f
Callum Rogers <callumrogers@hotmail.co.uk>
2012-05-08 16:12:03 +0100

    Initial fiddlings

---
+++ soop/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open Printf
+open Microsoft.FSharp.Reflection
+
+type expr =
+    | Foo of string * expr * int list
+    | Cat
+
+
+let rec fmt(x):string =
+    let t = x.GetType()
+    if t.Name.StartsWith("FSharpList") then
+        x.ToString()
+    else if FSharpType.IsUnion(t) then
+        let union, fields = FSharpValue.GetUnionFields(x, t)
+        if fields.Length = 0 then
+            union.Name
+        else
+            let fieldsStr = String.Join(" ", Array.map fmt fields)
+            let unionStr = sprintf "%s %s" union.Name fieldsStr       
+            sprintf "(%s)" unionStr
+    else
+        x.ToString()
+       
+
+
+let f = Foo ("lol", Foo("bar", Cat, [4;5;6]), [1; 2; 3])
+printfn "%s" (fmt f)
+printfn "%s" (fmt Cat)
