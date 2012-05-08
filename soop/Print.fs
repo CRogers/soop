fcd516348cb35bfd0334804d89d05ae7ca8269c4
Callum Rogers <callumrogers@hotmail.co.uk>
2012-05-08 16:55:53 +0100

    Fixed build process

---
+++ soop/Print.fs
@@ -0,0 +1,19 @@
+﻿module Print
+
+open Microsoft.FSharp.Reflection
+open System
+
+let rec fmt(x):string =
+    let t = x.GetType()
+    if t.Name.StartsWith("FSharpList") then
+        x.ToString()
+    else if FSharpType.IsUnion(t) then
+        let union, fields = FSharpValue.GetUnionFields(x, t)
+        if fields.Length = 0 then
+            union.Name
+        else
+            let fieldsStr = String.Join(" ", Array.map fmt fields)
+            let unionStr = sprintf "%s %s" union.Name fieldsStr       
+            sprintf "(%s)" unionStr
+    else
+        x.ToString()
