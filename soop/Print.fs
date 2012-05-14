d30cece24e15a106d30a1b2e760ec7642b7a671e
Callum Rogers <callumrogers@hotmail.co.uk>
2012-05-14 17:30:06 +0100

    Fixed List display problems for printing class

--- soop/Print.fs
+++ soop/Print.fs
@@ -3,17 +3,28 @@
 open Microsoft.FSharp.Reflection
 open System
 
-let rec fmt(x):string =
+let rec fmt_list(x):string =
     let t = x.GetType()
-    if t.Name.StartsWith("FSharpList") then
-        x.ToString()
-    else if FSharpType.IsUnion(t) then
+    let union, fields = FSharpValue.GetUnionFields(x, t)
+    match union.Name with
+        | "Empty" -> ""
+        | "Cons"  -> sprintf "%s; %s" (fmt fields.[0]) (fmt_list fields.[1])
+
+and fmt(x):string =
+    let t = x.GetType()
+    //if t.Name.StartsWith("FSharpList") then
+    //    x.ToString()
+    if FSharpType.IsUnion(t) then
         let union, fields = FSharpValue.GetUnionFields(x, t)
-        if fields.Length = 0 then
+        if union.Name = "Empty" then "[]"
+        elif union.Name = "Cons" then
+            sprintf "[%s]" (fmt_list x)
+        elif fields.Length = 0 then
             union.Name
         else
             let fieldsStr = String.Join(" ", Array.map fmt fields)
-            let unionStr = sprintf "%s %s" union.Name fieldsStr       
-            sprintf "(%s)" unionStr
+            sprintf "(%s %s)" union.Name fieldsStr
     else
         x.ToString()
+
+let printfmt x = Console.WriteLine(fmt x)
