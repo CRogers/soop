58c3427e81321f26b463fd25969cd18606f3e576
Callum Rogers <callumrogers@hotmail.co.uk>
2012-05-14 18:28:37 +0100

    Made methodCall not require all namespace/class/method, LocalVar can now have an expr to initialise it

--- soop/Tree.fs
+++ soop/Tree.fs
@@ -14,33 +14,33 @@ type expr =
     | Int of int
     | String of string
     | Binop of op * expr * expr
-    (* namespace, class, methodName, args*)
-    | MethodCall of string * string * string * string list
+    // methodName, args
+    | MethodCall of string * string list
 
 type stmt =
-    (* condition, ifpart, elsepart *)
+    // condition, ifpart, elsepart
     | If of expr * stmt list * stmt list
-    (* condition, loop code *)
+    // condition, loop code
     | While of expr * stmt list
 
 type accessModifer =
     | Public | Private | Protected
 
-(* name, type, initial value *)
+// name, type, initial value
 type arg = Arg of string * stype
 
-(* name, params, return type, stmts *)
+// name, params, return type, stmts
 type methodDecl = Method of string * arg list * stype * stmt list
 
 type classDecl = 
-    | LocalVar of string * accessModifer * stype * Object
+    | LocalVar of string * accessModifer * stype * expr
     | Method of string * accessModifer * arg list * stype * stmt list
     | Constructor of arg list * stmt list
 
 type classInterfaceDecl = 
-    (* local vars, constructors, methods *)
+    // local vars, constructors, methods 
     | Class of string * classDecl list
-    (* methods *)
+    // methods
     | Interface of methodDecl list
 
 type namespaceDecl = Namespace of string * classInterfaceDecl list
